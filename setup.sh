640aa784c9af38401f41c1fbac08e8b7f0c07d1d
flaviomidea <flaben10@gmail.com>
2026-07-20 11:51:00 -0300

    update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+
+echo "\
 [general]\n\
 email = \"flaben10@gmail.com\"\n\
-" > ~/.streamlit/credentials.tomlecho "\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
+enableCORS = false\n\
+enableXsrfProtection = false\n\
 port = $PORT\n\
+maxUploadSize = 500\n\
+maxMessageSize = 500\n\
 " > ~/.streamlit/config.toml
